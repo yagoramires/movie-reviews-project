3185be3aa4c5dfba920a45664fb8358d89ef0d33
yagox <yago.ramiresx@gmail.com>
2022-12-07 19:49:45 -0300

    Header adjustments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 
 // Components
 import Header from './components/Header';
+
+// Pages
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Routes>
-        <Route path='/' element={Home} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -18,36 +18,52 @@ const Header = () => {
   const user = false;
 
   return (
-    <Root className='flex justify-between items-center max-w-[1200px] w-[80%] mx-auto py-4'>
-      <h1 className='text-2xl font-bold'>
-        movie<span className='text-yellow-400'>Reviews</span>
-      </h1>
-      <List className='relative right-4'>
-        {!user && (
-          <Item className=''>
-            <Trigger className=''>
-              <GiHamburgerMenu size={20} className='text-yellow-400' />
-            </Trigger>
-            <Content className='flex flex-col'>
-              <Link to='/login'>Login</Link>
-              <Link to='/register'>Register</Link>
-            </Content>
-          </Item>
-        )}
-        {user && (
-          <Item>
-            <Trigger className=''>
-              <RiUserLine />
-            </Trigger>
-            <Content className=''>
-              <Link to='/profile'>Profile</Link>
-              <Link to='/favorites'>Favorites</Link>
-              <span onClick={() => {}}>Sign out</span>
-            </Content>
-          </Item>
-        )}
-      </List>
-    </Root>
+    <header className='shadow-md bg-gray-50 '>
+      <Root className='flex justify-between items-center max-w-[1200px] w-[80%] mx-auto py-4 '>
+        <Link
+          to='/'
+          className='text-2xl font-bold transition-all duration-300 hover:tracking-wider'
+        >
+          movie
+          <span className='text-yellow-400'>Reviews</span>
+        </Link>
+        <List className='relative top-[4px] '>
+          {!user && (
+            <Item className=''>
+              <Trigger className=''>
+                <GiHamburgerMenu size={20} className='text-yellow-400' />
+              </Trigger>
+              <Content className='absolute right-0 flex flex-col gap-2 p-4 shadow-md bg-gray-50'>
+                <Link
+                  to='/login'
+                  className='px-4 text-yellow-400 transition-all duration-200 hover:text-black hover:font-bold hover:border-l-4 hover:border-l-yellow-400'
+                >
+                  Login
+                </Link>
+                <Link
+                  to='/register'
+                  className='px-4 text-yellow-400 transition-all duration-200 hover:text-black hover:font-bold hover:border-l-4 hover:border-l-yellow-400'
+                >
+                  Register
+                </Link>
+              </Content>
+            </Item>
+          )}
+          {user && (
+            <Item>
+              <Trigger className=''>
+                <RiUserLine />
+              </Trigger>
+              <Content className=''>
+                <Link to='/profile'>Profile</Link>
+                <Link to='/favorites'>Favorites</Link>
+                <span onClick={() => {}}>Sign out</span>
+              </Content>
+            </Item>
+          )}
+        </List>
+      </Root>
+    </header>
   );
 };
 
