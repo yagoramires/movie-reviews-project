2b6b81a1759db5ed9f4f7d4064e74b3577713f79
yagox <yago.ramiresx@gmail.com>
2022-12-15 19:07:04 -0300

    user validation on comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { useComment } from '../hooks/useComment';
 import { useFetchData } from '../hooks/useFetchData';
 
+import { FaLock } from 'react-icons/fa';
 import { TiDelete } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
 
 const Comments = ({ id, user }) => {
   const [newComment, setNewComment] = useState('');
@@ -31,22 +33,29 @@ const Comments = ({ id, user }) => {
       <h2 className='pl-4 text-2xl font-bold border-l-8 border-yellow-400 lg:text-4xl '>
         Comments
       </h2>
-      <form className='relative' onSubmit={commentHandler}>
-        <input
-          value={newComment || ''}
-          onChange={(e) => {
-            setNewComment(e.target.value);
-          }}
-          type='text'
-          placeholder='Write your comment'
-          className='w-full p-2 text-lg border-2 rounded-lg outline-none lg:p-4 bg-slate-50'
-        />
-        <input
-          className='absolute right-0 px-10 py-2 text-lg font-bold text-white transition-all duration-300 bg-yellow-400 border-2 border-yellow-400 rounded-r-lg cursor-pointer lg:py-4 hover:bg-yellow-300 hover:border-yellow-300 hover:tracking-wider'
-          type='submit'
-          value='Send'
-        />
-      </form>
+      {user ? (
+        <form className='relative' onSubmit={commentHandler}>
+          <input
+            value={newComment || ''}
+            onChange={(e) => {
+              setNewComment(e.target.value);
+            }}
+            type='text'
+            placeholder='Write your comment'
+            className='w-full p-2 text-lg border-2 rounded-lg outline-none lg:p-4 bg-slate-50'
+          />
+          <input
+            className='absolute right-0 px-10 py-2 text-lg font-bold text-white transition-all duration-300 bg-yellow-400 border-2 border-yellow-400 rounded-r-lg cursor-pointer lg:py-4 hover:bg-yellow-300 hover:border-yellow-300 hover:tracking-wider'
+            type='submit'
+            value='Send'
+          />
+        </form>
+      ) : (
+        <p className='flex items-center justify-center gap-2 p-4 font-bold border-2 border-gray-500 rounded-md bg-slate-50 text-slate-400'>
+          <FaLock className='mt-[-4px]' /> <Link to='/login'>Log in</Link> to
+          comment
+        </p>
+      )}
       <div>
         {comments
           ?.sort((a, b) => b.order - a.order)
@@ -57,7 +66,7 @@ const Comments = ({ id, user }) => {
                 <p className='w-full py-2 overflow-hidden text-sm italic text-gray-700 break-words'>
                   "{comment.comments}"
                 </p>
-                {user.uid === comment.userId && (
+                {user && user.uid === comment.userId && (
                   <p
                     onClick={() => deleteComment(comment.id)}
                     className='text-2xl text-red-500'
